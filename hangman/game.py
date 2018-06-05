543e3ba23303bd964ebba30d908fd587bad28b43
Dylan Cruse <dylancruse@live.com>
2018-06-05 01:03:18 +0000

    Added comments

--- hangman/game.py
+++ hangman/game.py
@@ -30,13 +30,19 @@ class GuessWord(object):
         self.masked = '*' * len(guess_word_lower)
     
     def perform_attempt(self, guess_letter):
+        #Returns a GuessAttempt object with the letter guessed
+        #and corresponding miss/hit attributes
+        
         if len(guess_letter) > 1:
             raise InvalidGuessedLetterException('Only guess 1 character')
+            
         guess_letter_lower = guess_letter.lower()
         masked = self.masked.lower()
         answer = self.answer.lower()
+        
         if guess_letter_lower in answer:
             updated_masked = ''
+            #update the masked word with either the guessed letter or '*'
             for index, char in enumerate(answer):
                 if char == guess_letter_lower or char == masked[index]:
                     updated_masked += char
@@ -54,25 +60,40 @@ class HangmanGame(object):
     def __init__(self, word_list=None, number_of_guesses=5):
         self.remaining_misses = number_of_guesses
         self.previous_guesses = []
+        
+        #if word list provided, get a random word from that list.
+        #otherwise get a random word from WORD_LIST
         if word_list:
             random_word = self.select_random_word(word_list)
         else:
             random_word = self.select_random_word(self.WORD_LIST)
+        
+        #self.word contains the word to be guessed, also referred to as 'answer'
         self.word = GuessWord(random_word)
     
     def guess(self, guess_letter):
-        if self.is_finished():
-            raise GameFinishedException('Out of guesses')
-            
         guess_letter_lower = guess_letter.lower()
+        
+        #check if the game has already been completed
+        if self.is_finished():
+            raise GameFinishedException('Game already completed')
+        
+        #perform attempt through self.word and assign returned object to attempt
         attempt = self.word.perform_attempt(guess_letter_lower)
+        
+        #if a miss, decrement remaining guesses
         if attempt.is_miss():
             self.remaining_misses -= 1
+        
+        #add guess to previous guesses, check for duplicates
         if guess_letter_lower not in self.previous_guesses:
             self.previous_guesses.append(guess_letter_lower)
-            
+        
+        #check for a win
         if self.is_won():
             raise GameWonException('You win!')
+        
+        #check for a loss
         if self.is_lost():
             raise GameLostException('Better luck next time!')
             
